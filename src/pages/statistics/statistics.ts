ccaeaba3859e237c1030432767af43b7a4331ed7
Daniel Lemburg <daniel.lemburg1@gmail.com>
2017-09-30 22:02:49 -0700

    fixed some small bugs and added filtered table

--- src/pages/statistics/statistics.ts
+++ src/pages/statistics/statistics.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Api, ROUTES } from '../../utils/api';
+import { Utils } from '../../utils/utils';
 
 
 @IonicPage()
@@ -11,10 +12,12 @@ import { Api, ROUTES } from '../../utils/api';
 export class StatisticsPage {
   startDate: string = null;
   endDate: string = null;
+  months = Utils().months;
 
   state = {
     avgForecastHours: null,
-    avgActualHours: null
+    avgActualHours: null,
+    dates: []
   }
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public Api: Api) {
@@ -32,7 +35,14 @@ export class StatisticsPage {
       this.Api.call(ROUTES.getFilteredData + `?startDate=${this.startDate}&endDate=${this.endDate}`, "GET")
         .then((response) => {
           console.log("response.data: ", response.data);
-          this.state = response.data.filteredData;
+
+          if (response.data.filteredData.dates.length) {
+            this.state = {
+              avgForecastHours: response.data.filteredData.avgForecastHours,
+              avgActualHours: response.data.filteredData.avgActualHours,
+              dates: response.data.filteredData.dates
+            }
+          }
         })
         .catch((err) => {
           console.log("err: ", err);
